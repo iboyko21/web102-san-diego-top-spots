19740d547d53a1b7afa71a2a618566b1bdb81955
Igor Boyko <iboyko@gmail.com>
2022-05-19 17:09:55 -0700

    added styles, all the functionality works

--- main.js
+++ main.js
@@ -2,18 +2,27 @@ $(document).ready(function() {
     $.getJSON( "data.json", function( data ) {
         $.each( data, function( key, value ) {
             $("#output").append( 
-                `<tr id="${key+1}">
-                <td>${key+1}</td>
-                <td>${value.name}</td> 
-                <td>${value.description}</td>
-                <td>${value.location}</td> 
-                <td><a href="https://www.google.com/maps?q=${value.location[0]},${value.location[1]}" target="_blank">Open</a></td>
+                `<tr>
+                    <td>${key+1}</td>
+                    <td>${value.name}</td> 
+                    <td>${value.description}</td>
+                    <td><button data-lat="${value.location[0]}" data-lng="${value.location[1]}">Update Map</button></td>
                 </tr>` 
                 );
+
+       $(function() {
+           console.log("Function activated");
+                    $('button').each(function(i, btn) {
+                      $(btn).click(function() {
+                        var el = $(this)[0];
+                        // console.log("El: " + el);
+                        var lat=el.dataset.lat;
+                        var lng=el.dataset.lng;
+                        // $("iframe").attr("src", urlbeg + lat + "," + lng + urlend);
+                        $("iframe").attr("src", `https://maps.google.com/maps?q=${lat},${lng}&t=&z=12&ie=UTF8&iwloc=&output=embed`);
+                      });
+                    });
+                  });
         });
-      });
+    });
 });
-
-$("#test").append("This is a test text!");
-
-// https://www.google.com/maps?q=33.09745,-116.99572
